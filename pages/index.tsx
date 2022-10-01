a32bbc9849b0d933293a3be451696bffd38727eb
Fernando Sanchez Vilela <f.sanchezvilela@gmail.com>
2022-10-01 23:46:28 +0200

    feat(index): add basic ui structure

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,17 @@
 import type { NextPage } from "next";
 
 const Home: NextPage = () => {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <div className="h-screen w-screen flex flex-col justify-center align-center items-center">
+      <div className="text-2xl text-center">Which Pokémon is Rounder?</div>
+      <div className="p-2" />
+      <div className="border rounded p-8 flex items-center justify-between max-w-2xl">
+        <div className="w-16 h-16 bg-red-200" />
+        <div className="p-8">VS</div>
+        <div className="w-16 h-16 bg-red-200" />
+      </div>
+    </div>
+  );
 };
 
 export default Home;
